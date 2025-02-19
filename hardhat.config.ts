d7fe183a3277bb198a8e7b1aa4c9b2be27880a7d
vxxxxc <>
2025-02-19 23:50:54 +0800

    add missing dependency & add optimizer for fix deploying errors

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,19 @@
 import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-viem";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-foundry";
+import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version:"0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  },
   defaultNetwork: "hardhat"
 };
 
