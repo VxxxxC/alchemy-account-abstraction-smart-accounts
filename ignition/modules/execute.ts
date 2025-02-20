ed1199d7e07bb96c38db5529c263245ec2aa23bc
vxxxxc <>
2025-02-21 01:30:31 +0800

    prepare [userOperation] object for emit to [Entry Point]

--- ignition/modules/execute.ts
+++ ignition/modules/execute.ts
@@ -1,34 +1,41 @@
 import * as hre from "hardhat";
 
 const FACTORY_NONCE = 1;
-const ENTRY_POINT_ADDRESS = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
-const FACTORY_ADDRESS = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9";
+const ENTRY_POINT_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+const FACTORY_ADDRESS = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
 
-async function main(){
-  const entryPoint = await hre.viem.getContractAt("EntryPoint", ENTRY_POINT_ADDRESS);
-  console.log({entryPoint})
+async function main() {
+    const [signer] = await hre.ethers.getSigners();
+    const address = await signer.getAddress();
+    const entryPoint = await hre.ethers.getContractAt("EntryPoint", ENTRY_POINT_ADDRESS);
 
-  const sender = hre.ethers.getCreateAddress({from: FACTORY_ADDRESS, nonce: FACTORY_NONCE});
-  console.log({sender})
+    const sender = hre.ethers.getCreateAddress({ from: FACTORY_ADDRESS, nonce: FACTORY_NONCE });
+
     // CREATE: hash(deployer + nonce)
     // CREATE2: hash(0xFF + deployer + bytecode + salt)
 
+    const AccountFactory = await hre.ethers.getContractFactory("AccountFactory");
+    const Account = await hre.ethers.getContractFactory("Account");
+    const initCode = FACTORY_ADDRESS + AccountFactory.interface.encodeFunctionData("createAccount",[address]);
+
     const userOp = {
-         sender, // smart account address
-         nonce,
-         initCode,
-         callData,
-         callGasLimit: 200_00,
-         verificationGasLimit: 200_00,
-         preVerificationGas: 500_00,
-         maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
-         maxPriorityFeePerGas:hre.ethers.parseUnits("5", "gwei"),
-         paymasterAndData: "0x",
-         signature: "0x",
+        sender, // smart account address
+        nonce: await entryPoint.getNonce(sender),
+        initCode,
+        callData: Account.interface.encodeFunctionData("execute"),
+        callGasLimit: 200_00,
+        verificationGasLimit: 200_00,
+        preVerificationGas: 500_00,
+        maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
+        maxPriorityFeePerGas: hre.ethers.parseUnits("5", "gwei"),
+        paymasterAndData: "0x",
+        signature: "0x",
     }
+
+    console.log({userOp})
 }
 
 main().catch((err) => {
-  console.error(err);
-  process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1
 })
