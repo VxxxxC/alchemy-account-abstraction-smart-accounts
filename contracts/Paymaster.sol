3c6a5ef7614f38afcc1b9021efe07ab652f55232
vxxxxc <>
2025-03-06 02:30:19 +0800

    changed the paymaster.sol codes

--- contracts/Paymaster.sol
+++ contracts/Paymaster.sol
@@ -4,29 +4,22 @@ pragma solidity ^0.8.9;
 import "@account-abstraction/contracts/interfaces/IPaymaster.sol";
 
 contract Paymaster is IPaymaster {
-    /**
-     * payment validation: check if paymaster agrees to pay.
-     * Must verify sender is the entryPoint.
-     * Revert to reject this request.
-     * Note that bundlers will reject this method if it changes the state, unless the paymaster is trusted (whitelisted)
-     * The paymaster pre-pays using its deposit, and receive back a refund after the postOp method returns.
-     * @param userOp the user operation
-     * @param userOpHash hash of the user's request data.
-     * @param maxCost the maximum cost of this transaction (based on maximum gas and gas price from userOp)
-     * @return context value to send to a postOp
-     *      zero length to signify postOp is not required.
-     * @return validationData signature and time-range of this operation, encoded the same as the return value of validateUserOperation
-     *      <20-byte> sigAuthorizer - 0 for valid signature, 1 to mark signature failure,
-     *         otherwise, an address of an "authorizer" contract.
-     *      <6-byte> validUntil - last timestamp this operation is valid. 0 for "indefinite"
-     *      <6-byte> validAfter - first timestamp this operation is valid
-     *      Note that the validation code cannot use block.timestamp (or block.number) directly.
-     */
     function validatePaymasterUserOp(
-        UserOperation calldata userOp,
-        bytes32 userOpHash,
-        uint256 maxCost
-    ) external returns (bytes memory context, uint256 validationData) {}
+        UserOperation calldata,
+        bytes32,
+        uint256
+    ) external pure returns (bytes memory context, uint256 validationData) {
+        /*  NOTE: Paymaster Server
+                 20 bytes: paymasyer address
+                 timePeriod
+                 signature
+        
+        userOp.paymasterAndData
+        */
+
+        context = new bytes(0);
+        validationData = 0;
+    }
 
     /**
      * post-operation handler.
