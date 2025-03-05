31cbe22b1a0dc967aaf973496cfa329a534c2b2f
vxxxxc <>
2025-03-06 00:27:54 +0800

    start smart account - stage 2 lesson

--- ignition/modules/execute.ts
+++ ignition/modules/execute.ts
@@ -5,45 +5,53 @@ const ENTRY_POINT_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 const FACTORY_ADDRESS = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
 
 async function main() {
-    const [signer] = await hre.ethers.getSigners();
-    const address = await signer.getAddress();
-    const entryPoint = await hre.ethers.getContractAt("EntryPoint", ENTRY_POINT_ADDRESS);
+  const [signer] = await hre.ethers.getSigners();
+  const address = await signer.getAddress();
+  const entryPoint = await hre.ethers.getContractAt(
+    "EntryPoint",
+    ENTRY_POINT_ADDRESS
+  );
 
-    const sender = await hre.ethers.getCreateAddress({ from: FACTORY_ADDRESS, nonce: FACTORY_NONCE });
-    
-    await entryPoint.depositTo(sender, {
-        value: hre.ethers.parseEther("100")
-    })
+  const sender = hre.ethers.getCreateAddress({
+    from: FACTORY_ADDRESS,
+    nonce: FACTORY_NONCE,
+  });
 
-    // CREATE: hash(deployer + nonce)
-    // CREATE2: hash(0xFF + deployer + bytecode + salt)
+  await entryPoint.depositTo(sender, {
+    value: hre.ethers.parseEther("100"),
+  });
 
-    const AccountFactory = await hre.ethers.getContractFactory("AccountFactory");
-    const Account = await hre.ethers.getContractFactory("Account");
-    const initCode = FACTORY_ADDRESS + AccountFactory.interface.encodeFunctionData("createAccount",[address]).slice(2);
+  // CREATE: hash(deployer + nonce)
+  // CREATE2: hash(0xFF + deployer + bytecode + salt)
 
-    const userOp = {
-        sender, // smart account address
-        nonce: await entryPoint.getNonce(sender, 0),
-        initCode: initCode,
-        callData: Account.interface.encodeFunctionData("execute"),
-        callGasLimit: 200_00,
-        verificationGasLimit: 200_00,
-        preVerificationGas: 500_00,
-        maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
-        maxPriorityFeePerGas: hre.ethers.parseUnits("5", "gwei"),
-        paymasterAndData: "0x",
-        signature: "0x",
-    }
-    console.log(userOp.initCode.length)
-    console.log(userOp)
+  const AccountFactory = await hre.ethers.getContractFactory("AccountFactory");
+  const Account = await hre.ethers.getContractFactory("Account");
+  const initCode =
+    FACTORY_ADDRESS +
+    AccountFactory.interface
+      .encodeFunctionData("createAccount", [address])
+      .slice(2);
 
-    const tx = await entryPoint.handleOps([userOp], address);
-    const receipt = await tx.wait();
-    console.log({receipt});
+  const userOp = {
+    sender, // smart account address
+    nonce: await entryPoint.getNonce(sender, 0),
+    initCode,
+    callData: Account.interface.encodeFunctionData("execute"),
+    callGasLimit: 200_00,
+    verificationGasLimit: 200_00,
+    preVerificationGas: 500_00,
+    maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
+    maxPriorityFeePerGas: hre.ethers.parseUnits("5", "gwei"),
+    paymasterAndData: "0x",
+    signature: "0x",
+  };
+
+  const tx = await entryPoint.handleOps([userOp], address);
+  const receipt = await tx.wait();
+  console.log({ receipt });
 }
 
 main().catch((err) => {
-    console.error(err);
-    process.exitCode = 1
-})
+  console.error(err);
+  process.exitCode = 1;
+});
