159bd7f1c7e50446163374e19b07f06deea326a2
vxxxxc <>
2025-03-10 18:13:07 +0800

    add dotenv and dotenv config into hardhat config
    prepare to deploy on Arbitrum Sepolia testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,18 +3,27 @@ import "@nomicfoundation/hardhat-viem";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-foundry";
 import "@nomicfoundation/hardhat-ethers";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
-    version:"0.8.28",
+    version: "0.8.28",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 1000
-      }
-    }
+        runs: 1000,
+      },
+    },
   },
-  defaultNetwork: "localhost"
+  defaultNetwork: "localhost",
+  networks: {
+    arb: {
+      url: process.env.RPC_URL as string,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
 };
 
 export default config;
