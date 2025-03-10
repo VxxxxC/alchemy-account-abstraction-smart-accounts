fbdb52ecfb634ff6420a31de63cbdf5849ea42bf
vxxxxc <>
2025-03-10 23:16:28 +0800

    changed to use CREATE2

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "@account-abstraction/contracts/core/EntryPoint.sol";
 import "@account-abstraction/contracts/interfaces/IAccount.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/Create2.sol";
 import "hardhat/console.sol";
 
 contract Account is IAccount {
@@ -32,8 +33,19 @@ contract Account is IAccount {
 
 contract AccountFactory {
     function createAccount(address _owner) external returns (address) {
-        Account acc = new Account(_owner);
-        return address(acc);
+        bytes32 salt = bytes32(uint256(uint160(_owner))); //* NOTE: cast the address to bytes32 , this solution by Alchemy Don.
+        bytes memory bytecode = abi.encodePacked(
+            type(Account).creationCode,
+            abi.encode(_owner)
+        );
+
+        address addr = Create2.computeAddress(salt, keccak256(bytecode));
+        if (addr.code.length > 0) {
+            return addr;
+        }
+
+        //* NOTE: amount, salt, bytecode
+        return Create2.deploy(0, salt, bytecode);
     }
 }
 
