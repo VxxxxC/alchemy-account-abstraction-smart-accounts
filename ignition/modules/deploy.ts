7fe40f231624ac23d949e896ef0b8fcb09b13ffb
vxxxxc <>
2025-03-12 01:10:45 +0800

    remove entry point deploy, in order to use Alchemy entry point address

--- ignition/modules/deploy.ts
+++ ignition/modules/deploy.ts
@@ -1,18 +1,18 @@
 import * as hre from "hardhat";
 
-async function main(){
-
-  const entryPoint = await hre.viem.deployContract("EntryPoint");
-  console.log("Entry Point deployed at : ", entryPoint.address );
+async function main() {
+  // const entryPoint = await hre.viem.deployContract("EntryPoint");
+  // console.log("Entry Point deployed at : ", entryPoint.address );
 
   const accountFactory = await hre.viem.deployContract("AccountFactory");
-  console.log("Account Factory deployed at : ", accountFactory.address );
+  console.log("Account Factory deployed at : ", accountFactory.address);
 
   const paymaster = await hre.viem.deployContract("Paymaster");
-  console.log("Paymaster deployed at : ", paymaster.address );
+  console.log("Paymaster deployed at : ", paymaster.address);
 }
 
 main().catch((err) => {
   console.error(err);
-  process.exitCode = 1
-})
+  process.exitCode = 1;
+});
+
